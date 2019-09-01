4906d75c15e9b8d3f7ca4facb4f2ceee5742cc99
Sabir <sabir.iiui@gmail.com>
2019-09-01 21:27:55 +0500

    server in production 1

--- src/saga/index.js
+++ src/saga/index.js
@@ -1,10 +1,11 @@
 import { takeLatest, put } from "redux-saga/effects";
 import axios from 'axios'
-
+// const API_URL = 'http://localhost:3001/donors'
+const API_URL = `https://donors-list.herokuapp.com:${process.env.PORT || 3001}/donors`
 function* addDonorAsync(data) {
     //Call API Here
     const item = { ...data.value }
-    const addedItem = yield axios.post('http://localhost:3001/donors', item);
+    const addedItem = yield axios.post(API_URL, item);
     yield put({ type: "ADD_DONOR_ASYNC", value: addedItem.data });
 }
 
@@ -15,7 +16,7 @@ export function* watchAddDonor() {
 function* updateDonorAsync(data) {
     //Call API Here
     const item = { ...data.value }
-    const addedItem = yield axios.put(`http://localhost:3001/donors/${item.id}`, item);
+    const addedItem = yield axios.put(`${API_URL}/${item.id}`, item);
     yield put({ type: "UPDATE_DONOR_ASYNC", value: addedItem.data });
 }
 
@@ -24,9 +25,8 @@ export function* watchUpdateDonor() {
 }
 
 function* loadDonorAsync() {
-    const url = 'http://localhost:3001/donors'
     yield put({ type: "SET_LOADING", value: true });
-    const res = yield axios(url)
+    const res = yield axios(API_URL)
     yield put({ type: "LOAD_DONORS_ASYNC", value: res.data });
     yield put({ type: "SET_LOADING", value: false });
 
